660023f368376f043d2069a00b97215ecb6e3eae
Jakub Cichocki <cichykanal32@gmail.com>
2021-09-12 15:53:13 +0200

    Checking if score already exists

--- controllers/score.js
+++ controllers/score.js
@@ -6,30 +6,26 @@ exports.saveScore = async (req, res) => {
     name: req.body.name,
     score: req.body.score,
   });
-  const alreadyExists = await Score.findOne({ name: score.name, score: score.score }).exec()
-  if(alreadyExists)
-    {
-      res.send('Score already exists')
-    }else{
-      try {
-        const savedScore = await score.save();
-        res.send(savedScore);
-      } catch (error) {
-        res.send(error)
-      }
+  const alreadyExists = await Score.findOne({
+    name: score.name,
+    score: score.score,
+  }).exec();
+  if (alreadyExists) {
+    res.send("Score already exists");
+  } else {
+    try {
+      const savedScore = await score.save();
+      res.send(savedScore);
+    } catch (error) {
+      res.send(error);
     }
-
-
+  }
 };
 
 exports.getHighestScore = async (req, res) => {
   const user = req.body.name;
   try {
-    const highestScore = await Score.findOne({ name: user }).sort("-score")
-    res.json(highestScore.score)
-  } catch (error) {
-      
-  }
-
-
+    const highestScore = await Score.findOne({ name: user }).sort("-score");
+    res.json(highestScore.score);
+  } catch (error) {}
 };
